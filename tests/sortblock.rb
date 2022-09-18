28f1e251a7c2e5e12ded8a587bb754a091711854
onli <onli@paskuda.biz>
2022-09-18 15:38:27 +0200

    Add sortblock test

---
+++ tests/sortblock.rb
@@ -0,0 +1,43 @@
+require_relative '../block.rb'
+require_relative "../blocks/sortblock.rb"
+require "test/unit"
+ 
+class TestSortBlock < Test::Unit::TestCase
+ 
+    def test_empty_feed
+        sortblock = Sortblock.new
+        sortblock.options = {:userinputs => ['updated', 'desc']}
+        empty_feed = '<rss version="2.0"><channel><title>An empty feed</title><link></link><description>No items.</description></channel></rss>'
+        assert_equal('<rss version="2.0"><channel><title>Nothing to sort</title><link></link><description>The input feed contained no items.</description></channel></rss>', sortblock.process([empty_feed]))
+    end
+
+    
+    def test_newest_first
+        sortblock = Sortblock.new
+        sortblock.options = {:userinputs => ['published', 'desc']}
+        test_feed = '<rss version="2.0">
+                <channel>
+                <title>title</title><link></link><description>Three items.</description>
+                <item><title>First</title><link>https://www.example.com/</link><pubDate>Sun, 18 Sep 2022 04:37:58 +0000</pubDate><description>ABC</description></item>
+                <item><title>Third</title><link>https://www.example.com/</link><pubDate>Tue, 20 Sep 2022 04:37:58 +0000</pubDate><description>ABC</description></item>
+                <item><title>Second</title><link>https://www.example.com/</link><pubDate>Mon, 19 Sep 2022 04:37:58 +0000</pubDate><description>ABC</description></item>
+            </channel>
+        </rss>'
+        assert_equal(['<pubDate>Thu, 01 Jan 1970 01:00:00 +0100</pubDate>', '<pubDate>Tue, 20 Sep 2022 04:37:58 -0000</pubDate>', '<pubDate>Mon, 19 Sep 2022 04:37:58 -0000</pubDate>', '<pubDate>Sun, 18 Sep 2022 04:37:58 -0000</pubDate>'], sortblock.process([test_feed]).scan(/<pubDate>.*<\/pubDate>/))
+    end
+
+    def test_oldest_first
+        sortblock = Sortblock.new
+        sortblock.options = {:userinputs => ['published', 'asc']}
+        test_feed = '<rss version="2.0">
+                <channel>
+                <title>title</title><link></link><description>Three items.</description>
+                <item><title>First</title><link>https://www.example.com/</link><pubDate>Sun, 18 Sep 2022 04:37:58 +0000</pubDate><description>ABC</description></item>
+                <item><title>Third</title><link>https://www.example.com/</link><pubDate>Tue, 20 Sep 2022 04:37:58 +0000</pubDate><description>ABC</description></item>
+                <item><title>Second</title><link>https://www.example.com/</link><pubDate>Mon, 19 Sep 2022 04:37:58 +0000</pubDate><description>ABC</description></item>
+            </channel>
+        </rss>'
+        assert_equal(['<pubDate>Thu, 01 Jan 1970 01:00:00 +0100</pubDate>', '<pubDate>Sun, 18 Sep 2022 04:37:58 -0000</pubDate>', '<pubDate>Mon, 19 Sep 2022 04:37:58 -0000</pubDate>', '<pubDate>Tue, 20 Sep 2022 04:37:58 -0000</pubDate>'], sortblock.process([test_feed]).scan(/<pubDate>.*<\/pubDate>/))
+    end
+ 
+end
